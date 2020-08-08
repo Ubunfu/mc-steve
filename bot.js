f38a710b4d7cd36ab56dec5ee79fa4eafefa2b6f
Ryan Allen <rallen3882@gmail.com>
2020-08-08 00:45:47 -0400

    Check roles before allowing command execution

--- bot.js
+++ bot.js
@@ -9,8 +9,44 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+async function msgAuthorHasRole(msg, opRole) {
+
+    let resultRole = msg.guild.roles.cache
+        .find(role => role.name === opRole);
+    
+    if (resultRole === undefined) { return false; }
+    
+    let opRoleMembers = resultRole.members;
+
+    if (opRoleMembers.some(guildMember => guildMember.user.username === msg.author.username)) {
+        return true;
+    }
+
+    return false;
+}
+
+async function tryStartServer(msg, opRole) {
+    if (await msgAuthorHasRole(msg, opRole)) {
+        msg.reply("Starting the server");
+    } else {
+        msg.reply("You need the " + opRole + " role to do that");
+    }
+}
+
 client.on('message', msg => {
+
+    const OP_ROLE = process.env.OP_ROLE;
+
+    if (msg.content === "!s") {
+        tryStartServer(msg, OP_ROLE)
+            .catch((err) => {
+                console.log(err);
+                msg.reply("Sorry, something went wrong");
+            });
+    }
+    
     if (msg.content === '!up') {
+
         axios.get('https://httpbin.org/status/200')
             .then(res => {
                 console.log(res.data);
