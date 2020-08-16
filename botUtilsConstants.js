4c28268a8d0a983d8a7301d648d904d18a7cbd00
Ryan Allen <rallen3882@gmail.com>
2020-08-16 11:02:47 -0400

    Updating the help menu

--- botUtilsConstants.js
+++ botUtilsConstants.js
@@ -11,6 +11,7 @@ const UNKNOWN_COMMAND_REPLIES = [
 const HELP_MESSAGE = 'This is what I can do: \n'
     + '* `start`: I\'ll start up our Minecraft server \n'
     + '* `stop`: I\'ll stop our Minecraft server \n'
+    + '* `search <search terms>`: I\'ll search the Minecraft Wiki\n' 
     + '* `help`: Show this help, since as it stands you can\'t do anything for yourself';
 
 const EMPTY_SEARCH_RESULTS_MESSAGE = "No results!";
