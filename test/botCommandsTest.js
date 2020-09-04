f9157d53693eba2cb6d147e89f2e2d670b5f07e4
Ryan Allen <rallen3882@gmail.com>
2020-09-04 01:22:33 -0400

    Refactoring bot command integration tests

--- test/botCommandsTest.js
+++ test/botCommandsTest.js
@@ -9,13 +9,13 @@ const awsHelpersTestConstants = require('./awsHelpersTestConstants.js');
 const axios = require('axios');
 const botUtils = require('../botUtils.js');
 
-describe('giveHelp(msg)', function() {
-    it('Should reply with correct message', async function() {
-        const message = new discord.Message();
-        const replyStub = sinon
-            .stub(message, "reply")
-            .returns(null);
 
+describe('giveHelp(msg)', function() {
+    const message = new discord.Message();
+    const replyStub = sinon
+        .stub(message, "reply")
+        .returns(null);
+    it('Should reply with correct message', async function() {
         await botCommands.giveHelp(message);
 
         expect(replyStub.calledOnce).to.be.true;
@@ -25,269 +25,257 @@ describe('giveHelp(msg)', function() {
 });
 
 describe('tryStartServer(msg)', function() {
+    let botAuthStub, replyStub = null;
+    const message = new discord.Message();
+    beforeEach(function() {
+        replyStub = sinon
+            .stub(message, "reply")
+            .returns(null);
+    });
+    afterEach(function() {
+        replyStub.restore();
+    });
+    
     describe('When message author is not privileged', function() {
-        it('Should not try to start server', async function() {
-            const botAuthStub = sinon
+        let awsStartSpy = null;
+        beforeEach(function() {
+            botAuthStub = sinon
                 .stub(botAuthenticator, "msgAuthorIsPrivileged")
                 .returns(false);
-            const awsStartSpy = sinon
-                .spy(awsHelpers, "startServer");
-
-            await botCommands.tryStartServer(new discord.Message());
-
-            expect(awsStartSpy.callCount).to.be.equal(0);
+            awsStartSpy = sinon
+                .spy(awsHelpers, "startServer"); 
+        });
+        afterEach(function() {
             botAuthStub.restore();
             awsStartSpy.restore();
+        });
+
+        it('Should not try to start server', async function() {
+            await botCommands.tryStartServer(new discord.Message());
+            expect(awsStartSpy.callCount).to.be.equal(0);
         });
     });
 
     describe('When message author is privileged', function() {
-        it('Should try to start server', async function() {
-            const botAuthStub = sinon
+        let awsStartStub = null;
+        beforeEach(function() {
+            botAuthStub = sinon
                 .stub(botAuthenticator, "msgAuthorIsPrivileged")
                 .returns(true);
-            const awsStartStub = sinon
+        });
+        afterEach(function() {
+            botAuthStub.restore();
+        });
+
+        it('Should try to start server', async function() {
+            awsStartStub = sinon
                 .stub(awsHelpers, "startServer")
                 .resolves(null);
             
             await botCommands.tryStartServer(new discord.Message());
 
             expect(awsStartStub.calledOnce).to.be.true;
-            botAuthStub.restore();
             awsStartStub.restore();
+        });
+
+        describe('When AWS start returns server in \'starting\' state', function() {
+            beforeEach(function() {
+                awsStartStub = sinon
+                    .stub(awsHelpers, "startServer")
+                    .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_STARTING);
+            });
+            afterEach(function() {
+                awsStartStub.restore();
+            });
+
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStartServer(message);
+                expect(replyStub.calledOnceWith('Starting server')).to.be.true;
+            });
+        });
+
+        describe('When AWS start returns server in \'pending\' state', function() {
+            beforeEach(function() {
+                awsStartStub = sinon
+                    .stub(awsHelpers, "startServer")
+                    .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_PENDING);
+            });
+            afterEach(function() {
+                awsStartStub.restore();
+            });
+
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStartServer(message);
+                expect(replyStub.calledOnceWith('Starting server')).to.be.true;
+            });
+        });
+
+        describe('When AWS start returns server in \'running\' state', function() {
+            beforeEach(function() {
+                awsStartStub = sinon
+                    .stub(awsHelpers, "startServer")
+                    .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_RUNNING);
+            });
+            afterEach(function() {
+                awsStartStub.restore();
+            });
+
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStartServer(message);
+                expect(replyStub.calledOnceWith('The server is already running')).to.be.true;
+            });
+        });
+
+        describe('When AWS start returns unexpected response', function() {
+            beforeEach(function() {
+                awsStartStub = sinon
+                    .stub(awsHelpers, "startServer")
+                    .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_UNEXPECTED);
+            });
+            afterEach(function() {
+                awsStartStub.restore();
+            });
+
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStartServer(message);
+                expect(replyStub.calledOnceWith('I couldn\'t start the server')).to.be.true;
+            });
+        });
+
+        describe('When AWS start returns an error', function() {
+            beforeEach(function() {
+                awsStartStub = sinon
+                    .stub(awsHelpers, "startServer")
+                    .rejects();
+            });
+            afterEach(function() {
+                awsStartStub.restore();
+            });
+
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStartServer(message);
+                expect(replyStub.calledOnceWith('Technical problem starting the server')).to.be.true;
+            });
         });
     });
 
-    describe('When AWS start returns server in \'starting\' state', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStartStub = sinon
-                .stub(awsHelpers, "startServer")
-                .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_STARTING);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStartServer(message);
-
-            expect(replyStub.calledOnceWith('Starting server')).to.be.true;
-            botAuthStub.restore();
-            awsStartStub.restore();
-            replyStub.restore();
-        });
-    });
-
-    describe('When AWS start returns server in \'pending\' state', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStartStub = sinon
-                .stub(awsHelpers, "startServer")
-                .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_PENDING);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStartServer(message);
-
-            expect(replyStub.calledOnceWith('Starting server')).to.be.true;
-            botAuthStub.restore();
-            awsStartStub.restore();
-            replyStub.restore();
-        });
-    });
-
-    describe('When AWS start returns server in \'running\' state', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStartStub = sinon
-                .stub(awsHelpers, "startServer")
-                .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_RUNNING);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStartServer(message);
-
-            expect(replyStub.calledOnceWith('The server is already running')).to.be.true;
-            botAuthStub.restore();
-            awsStartStub.restore();
-            replyStub.restore();
-        });
-    });
-
-    describe('When AWS start returns unexpected response', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStartStub = sinon
-                .stub(awsHelpers, "startServer")
-                .resolves(awsHelpersTestConstants.EC2_START_INSTANCES_RESPONSE_UNEXPECTED);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStartServer(message);
-
-            expect(replyStub.calledOnceWith('I couldn\'t start the server')).to.be.true;
-            botAuthStub.restore();
-            awsStartStub.restore();
-            replyStub.restore();
-        });
-    });
-
-    describe('When AWS start returns error', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStartStub = sinon
-                .stub(awsHelpers, "startServer")
-                .rejects(null);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStartServer(message);
-
-            expect(replyStub.calledOnceWith('Technical problem starting the server')).to.be.true;
-            botAuthStub.restore();
-            awsStartStub.restore();
-            replyStub.restore();
-        });
-    });
 });
 
 describe('tryStopServer(msg)', function() {
+    let botAuthStub, replyStub = null;
+    const message = new discord.Message();
+    beforeEach(function() {
+        replyStub = sinon
+            .stub(message, "reply")
+            .returns(null);
+    });
+    afterEach(function() {
+        replyStub.restore();
+    });
+
     describe('When message author is not privileged', function() {
-        it('Should not try to stop server', async function() {
-            const botAuthStub = sinon
+        let awsStopSpy = null;
+        beforeEach(function() {
+            botAuthStub = sinon
                 .stub(botAuthenticator, "msgAuthorIsPrivileged")
                 .returns(false);
-            const awsStopSpy = sinon
-                .spy(awsHelpers, "stopServer");
-
-            await botCommands.tryStopServer(new discord.Message());
-
-            expect(awsStopSpy.callCount).to.be.equal(0);
+            awsStopSpy = sinon
+                .spy(awsHelpers, "stopServer"); 
+        });
+        afterEach(function() {
             botAuthStub.restore();
             awsStopSpy.restore();
+        });
+        it('Should not try to stop server', async function() {
+            await botCommands.tryStopServer(new discord.Message());
+            expect(awsStopSpy.callCount).to.be.equal(0);
         });
     });
 
     describe('When message author is privileged', function() {
-        it('Should try to stop server', async function() {
-            const botAuthStub = sinon
+        let awsStopStub = null;
+        beforeEach(function() {
+            botAuthStub = sinon
                 .stub(botAuthenticator, "msgAuthorIsPrivileged")
                 .returns(true);
-            const awsStopStub = sinon
+        });
+        afterEach(function() {
+            botAuthStub.restore();
+        });
+
+        it('Should try to stop server', async function() {
+            awsStopStub = sinon
                 .stub(awsHelpers, "stopServer")
                 .resolves(null);
             
             await botCommands.tryStopServer(new discord.Message());
 
             expect(awsStopStub.calledOnce).to.be.true;
-            botAuthStub.restore();
             awsStopStub.restore();
         });
-    });
 
-    describe('When AWS stop returns server in \'stopping\' state', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStopStub = sinon
-                .stub(awsHelpers, "stopServer")
-                .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_STOPPING);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStopServer(message);
+        describe('When AWS stop returns server in \'stopping\' state', function() {
+            beforeEach(function() {
+                awsStopStub = sinon
+                    .stub(awsHelpers, "stopServer")
+                    .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_STOPPING);
+            });
+            afterEach(function() {
+                awsStopStub.restore();
+            });
 
-            expect(replyStub.calledOnceWith('Stopping server')).to.be.true;
-            botAuthStub.restore();
-            awsStopStub.restore();
-            replyStub.restore();
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStopServer(message);
+                expect(replyStub.calledOnceWith('Stopping server')).to.be.true;
+            });
         });
-    });
 
-    describe('When AWS stop returns server in \'stopped\' state', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStopStub = sinon
-                .stub(awsHelpers, "stopServer")
-                .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_STOPPED);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStopServer(message);
+        describe('When AWS stop returns server in \'stopped\' state', function() {
+            beforeEach(function() {
+                awsStopStub = sinon
+                    .stub(awsHelpers, "stopServer")
+                    .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_STOPPED);
+            });
+            afterEach(function() {
+                awsStopStub.restore();
+            });
 
-            expect(replyStub.calledOnceWith('The server is already stopped')).to.be.true;
-            botAuthStub.restore();
-            awsStopStub.restore();
-            replyStub.restore();
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStopServer(message);
+                expect(replyStub.calledOnceWith('The server is already stopped')).to.be.true;
+            });
         });
-    });
 
-    describe('When AWS stop returns unexpected response', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStopStub = sinon
-                .stub(awsHelpers, "stopServer")
-                .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_UNEXPECTED);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStopServer(message);
+        describe('When AWS stop returns unexpected response', function() {
+            beforeEach(function() {
+                awsStopStub = sinon
+                    .stub(awsHelpers, "stopServer")
+                    .resolves(awsHelpersTestConstants.EC2_STOP_INSTANCES_RESPONSE_UNEXPECTED);
+            });
+            afterEach(function() {
+                awsStopStub.restore();
+            });
 
-            expect(replyStub.calledOnceWith('I couldn\'t stop the server')).to.be.true;
-            botAuthStub.restore();
-            awsStopStub.restore();
-            replyStub.restore();
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStopServer(message);
+                expect(replyStub.calledOnceWith('I couldn\'t stop the server')).to.be.true;
+            });
         });
-    });
 
-    describe('When AWS stop returns error', function() {
-        it('Should reply with correct message', async function() {
-            const botAuthStub = sinon
-                .stub(botAuthenticator, "msgAuthorIsPrivileged")
-                .returns(true);
-            const awsStopStub = sinon
-                .stub(awsHelpers, "stopServer")
-                .rejects(null);
-            const message = new discord.Message();
-            const replyStub = sinon
-                .stub(message, "reply")
-                .returns(null);
-            
-            await botCommands.tryStopServer(message);
+        describe('When AWS stop returns error', function() {
+            beforeEach(function() {
+                awsStopStub = sinon
+                    .stub(awsHelpers, "stopServer")
+                    .rejects();
+            });
+            afterEach(function() {
+                awsStopStub.restore();
+            });
 
-            expect(replyStub.calledOnceWith('Technical problem stopping the server')).to.be.true;
-            botAuthStub.restore();
-            awsStopStub.restore();
-            replyStub.restore();
+            it('Should reply with correct message', async function() {
+                await botCommands.tryStopServer(message);
+                expect(replyStub.calledOnceWith('Technical problem stopping the server')).to.be.true;
+            });
         });
     });
 });
